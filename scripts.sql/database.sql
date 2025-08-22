05ce2a7485bcc2d1e9a57b925299d577c05f953c
Matheus de Souza Silva <matheus1882014@outlook.com>
2025-08-21 23:49:17 -0300

    desenvolvimentoinicial

---
+++ scripts.sql/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Categorias;
+
+
+CREATE TABLE filmes (
+    id SERIAL PRIMARY KEY,
+    Vingadores_Era_de_ultron VARCHAR(255) NOT NULL,
+    Ação VARCHAR(50)
+);
+
+ALTER TABLE filmes
+ADD COLUMN "https://www.youtube.com/embed/Tg8hx9JKEZs?si=HRuGYwgyh0-cqUAn" VARCHAR(255);
